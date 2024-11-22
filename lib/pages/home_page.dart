cea697be032f614776b4799b168c12fa8ae4f167
bauyrzhan <newnewernewest123@gmail.com>
2024-11-22 13:23:54 +0500

    come back if u stuck in errors

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:swipe_cards/draggable_card.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 import 'dart:math';
+//come back 
+
 class Content {
   final String text;
   // final Color color;
