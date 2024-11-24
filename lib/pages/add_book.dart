c5848d0fb6175676d1d52f585845fb8232a264d4
bauyrzhan <newnewernewest123@gmail.com>
2024-11-24 15:41:33 +0500

    add book file

---
+++ lib/pages/add_book.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class AddBook extends StatefulWidget {
+  const AddBook({super.key});
+
+  @override
+  State<AddBook> createState() => _AddBookState();
+}
+
+class _AddBookState extends State<AddBook> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
