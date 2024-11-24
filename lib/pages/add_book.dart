2bead4bf1f198885ca906c5f1f82df2f92154e12
bauyrzhan <newnewernewest123@gmail.com>
2024-11-24 21:16:41 +0500

    image picker future func from gallery

--- lib/pages/add_book.dart
+++ lib/pages/add_book.dart
@@ -1,7 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-
 
 class AddBook extends StatefulWidget {
   const AddBook({super.key});
@@ -11,59 +11,76 @@ class AddBook extends StatefulWidget {
 }
 
 class _AddBookState extends State<AddBook> {
+  File? _selectedImage;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Container(
-        alignment: Alignment.bottomLeft,
-        child: Row(
-          // alignment: Alignment.bottomLeft,
+      appBar: AppBar(
+        title: const Text('Add Book'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
           children: [
-            
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                
-                color: Colors.purple,
-                width: 150,
-                height: 150,
-              
-                child: Center(child: InkWell
-                (
-                  child:
-                   Text("Select from Galary", style: TextStyle(
-                    color: Colors.white
-                   ),))
-                   ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                
-                color: Colors.purple,
-                width: 150,
-                height: 150,
-              
-                child: Center(child: 
-                InkWell
-                (
-                  child:
-                   Text("Take a photo", style: TextStyle(
-                    color: Colors.white
-                   ),))
-                   ),
-              ),
+            _selectedImage != null
+                ? Image.file(
+                    _selectedImage!,
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  )
+                : Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.grey[300],
+                    child: const Center(
+                      child: Text('No Image Selected'),
+                    ),
+                  ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: _pickImageFromGallery,
+                  icon: const Icon(Icons.photo),
+                  label: const Text('Select from Gallery'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: _pickImageFromCamera,
+                  icon: const Icon(Icons.camera_alt),
+                  label: const Text('Take a Photo'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
   }
-//image picker future func
-  Future _pickImageFromGallery() async{
-    await ImagePicker();
+
+  Future<void> _pickImageFromGallery() async {
+    final XFile? returnedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnedImage != null) {
+      setState(() {
+        _selectedImage = File(returnedImage.path);
+      });
+    }
   }
 
+  Future<void> _pickImageFromCamera() async {
+    final XFile? returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnedImage != null) {
+      setState(() {
+        _selectedImage = File(returnedImage.path);
+      });
+    }
+  }
 }
