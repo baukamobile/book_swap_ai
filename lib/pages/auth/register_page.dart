614897f7075982f90daba6a8f8c54306f2052869
bauyrzhan <newnewernewest123@gmail.com>
2024-12-03 21:36:16 +0500

    some errors

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,9 +1,8 @@
 import 'package:book_swap_ai/main.dart';
 import 'package:book_swap_ai/pages/auth/login_page.dart';
-import 'package:book_swap_ai/pages/chat_page.dart';
-import 'package:book_swap_ai/pages/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -13,9 +12,48 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  void login(context){
-    Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context)=>LoginPage())); // Use pushReplacement here
+  final _emailController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<void> register() async {
+    String email = _emailController.text;
+    String username = _nameController.text;
+    String password = _passwordController.text;
+
+    // Send HTTP POST request to Django API
+    try {
+      final response = await http.post(
+        Uri.parse('https://testbackendflutter-0471b16deb32.herokuapp.com/api/register/'),
+        body: json.encode({
+          'email': email,
+          'username': username,
+          'password': password,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 201) {
+        // Successful registration, navigate to login page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      } else {
+        // Handle registration error
+        final error = json.decode(response.body)['error'];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registration failed: $error')),
+        );
+      }
+    } catch (e) {
+      print('Error: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to connect to server')),
+      );
+    }
   }
 
   @override
@@ -29,10 +67,16 @@ class _RegisterPageState extends State<RegisterPage> {
               padding: EdgeInsets.symmetric(horizontal: 17),
               child: Align(
                 alignment: Alignment.topRight,
-                child: IconButton(onPressed: (){
-                  Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context)=>MyApp()));
-                }, icon: Icon(Icons.close))),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                    );
+                  },
+                  icon: Icon(Icons.close),
+                ),
+              ),
             ),
             Expanded(
               child: Column(
@@ -40,50 +84,48 @@ class _RegisterPageState extends State<RegisterPage> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text('Register',style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 33
-                    ),),
+                    child: Text(
+                      'Register',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 33),
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Email",
-                          enabledBorder: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(),
-                        ),
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        enabledBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20,),
+                  const SizedBox(height: 20),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Name",
-                          enabledBorder: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(),
-                        ),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        hintText: "Name",
+                        enabledBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20,),
+                  const SizedBox(height: 20),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          enabledBorder: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(),
-                        ),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        enabledBorder: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20,),
+                  const SizedBox(height: 20),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
@@ -92,22 +134,33 @@ class _RegisterPageState extends State<RegisterPage> {
                           height: 50,
                           width: 200,
                           color: Colors.green,
-                          child: TextButton(onPressed: (){}, child: Text("Register", style: TextStyle(
-                              color: Colors.white,
-                          ))),
+                          child: TextButton(
+                            onPressed: register,
+                            child: Text(
+                              "Register",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
                         ),
-                        const SizedBox(height: 20,),
+                        const SizedBox(height: 20),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text("Already have an account? "),
-                            TextButton(onPressed: (){
-                              login(context);
-                            }, child: Text("Log in", style: TextStyle(
-                              color: Colors.green,
-                            ),))
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => LoginPage()),
+                                );
+                              },
+                              child: Text(
+                                "Log in",
+                                style: TextStyle(color: Colors.green),
+                              ),
+                            ),
                           ],
-                        )
+                        ),
                       ],
                     ),
                   ),
