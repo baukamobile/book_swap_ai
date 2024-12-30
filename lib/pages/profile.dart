ee2aa1f1e33d6ecee236ad54ccbf85c0d34150a3
bauyrzhan <newnewernewest123@gmail.com>
2024-12-30 21:52:02 +0500

    while profile page doesnt change

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,55 @@
+import 'package:book_swap_ai/components/bottomnavbar.dart';
+import 'package:book_swap_ai/main.dart';
+import 'package:book_swap_ai/pages/auth/login_page.dart';
+import 'package:book_swap_ai/pages/auth/register_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+class Book {
+  final String title;
+  final String author;
+  final String description;
+  final String condition;
+  final String imageUrl;
+  final String owner;
+
+  Book({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.condition,
+    required this.imageUrl,
+    required this.owner,
+  });
+
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+        title: json['title'],
+        author: json['author'],
+        description: json['description'],
+        condition: json['condition'],
+        imageUrl: json['image'],
+        // Image URL returned by API
+        owner: json['owner']['name']);
+  }
+}
+class User{
+  final String name;
+  final String email;
+  
+  User({
+    required this.name,
+    required this.email,
+  });
+  factory User.fromJson(Map<String, dynamic> json){
+    return User(
+      name: json['name'],
+      email: json['email'],
+      );
+  }
+}
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -10,91 +58,163 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String name = "Tom";
-  String email = "tom@gmail.com";
-  String jwtToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIsImV4cCI6MTczNTU4MTc3MiwiaWF0IjoxNzM1NTc2MzcyfQ.Z_xJ4hug1EdLiI12QU6c2UMrVNFPcgSjFb5BDNS5Ccs"; // Example JWT token
-
-  final _nameController = TextEditingController();
-  final _emailController = TextEditingController();
-
+  List<Book> books = [];
+  List<User> user = [];
   @override
   void initState() {
     super.initState();
-    _nameController.text = name;
-    _emailController.text = email;
+    fetchBooks();
   }
 
-  Future<void> updateProfile() async {
-    final url = 'https://your-django-api-url/api/update-profile/';
-
+  Future<void> fetchBooks() async {
     try {
-      final response = await http.put(
-        Uri.parse(url),
-        headers: {
-          'Authorization': 'Bearer $jwtToken', // Pass the JWT token for authentication
-          'Content-Type': 'application/json',
-        },
-        body: json.encode({
-          'username': _nameController.text,
-          'email': _emailController.text,
-        }),
-      );
+      final response = await http.get(Uri.parse(
+          'https://testbackendflutter-0471b16deb32.herokuapp.com/api/books/18/'));
 
+      print('Status Code: ${response.statusCode}');
+      print('Response Body: ${response.body}');
       if (response.statusCode == 200) {
-        final responseData = json.decode(response.body);
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(responseData['message'] ?? 'Profile updated!'),
-        ));
+        final Map<String, dynamic> data = json.decode(response.body);
         setState(() {
-          name = _nameController.text;
-          email = _emailController.text;
+          books = [Book.fromJson(data)];
         });
       } else {
-        throw Exception('Failed to update profile');
+        throw Exception('Failed to load book: ${response.statusCode}');
       }
-    } catch (error) {
-      print(error);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Error: $error'),
-      ));
+    } catch (e) {
+      print('Error: $e');
     }
   }
 
+  Future<void> getUserName() async{
+    try {
+      final response = await http.get(Uri.parse(
+        'https://testbackendflutter-0471b16deb32.herokuapp.com/api/user/'
+      ),
+      );
+      print('Status Code: ${response.statusCode}');
+      print('Response Body: ${response.body}');
+      if(response.statusCode == 200){
+        final Map<String,dynamic> data = json.decode(response.body);
+        setState(() {
+          user = [User.fromJson(data)];
+        });
+      }else{
+        throw Exception('Error with network');
+      }
+    }catch(e){
+      print('Error: $e');
+    }
+  }
+
+  String name = "Tom";
+  String email = "tom@gmail.com";
+  var phoneNumber = 77775456787;
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/img/avtr.jpg'),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceBetween, // Adjust horizontal alignment
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Adjust vertical alignment
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                      },
+                      icon: Icon(Icons.bookmark),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => LoginPage()));
+                      },
+                      icon: Icon(Icons.exit_to_app),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  CircleAvatar(
+                    radius: 75,
+                    backgroundColor: Colors.grey[200],
+                    backgroundImage: AssetImage('assets/img/avtr.jpg'),
+                  ),
+                  const SizedBox(height: 20),
+                  
+                  _buildProfileDetailRow("Name:", name),
+                  _buildProfileDetailRow("Email:", email),
+                  _buildProfileDetailRow(
+                      "Phone Number:", phoneNumber.toString()),
+                  const SizedBox(height: 20),
+                  Container(
+                    child: Text("Active Books"),
+                  ),
+                  const SizedBox(height: 10),
+                  books.isNotEmpty
+                      ? SizedBox(
+                          height: 200, // Adjust height as needed
+                          child: ListView.builder(
+                            itemCount: books.length,
+                            itemBuilder: (context, index) {
+                              final book = books[index];
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => HomePage()));
+                                },
+                                child: Card(
+                                  child: ListTile(
+                                    leading: Image.network(book.imageUrl),
+                                    title: Text(book.title),
+                                    subtitle: Text(
+                                        '${book.author}\nCondition: ${book.condition} \n Owner: ${book.owner}'),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      : const Center(child: Text('No books.')),
+                ],
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: updateProfile,
-              child: const Text('Update Profile'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      // bottomNavigationBar: Bottomnavbar(),
     );
+  }
+
+  Widget _buildProfileDetailRow(String label, String value) {
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Text('$label '),
+            Text(value),
+          ],
+        ),
+        IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+      ],
+    );
+
   }
 }
